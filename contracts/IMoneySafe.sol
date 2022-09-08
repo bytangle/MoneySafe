028e2436d988727ee04e25bf7d9b3b91455e04c1
Bytangle <dev.bytangle@gmail.com>
2022-09-08 17:06:31 +0100

    Update IMoneySafe.sol

--- contracts/IMoneySafe.sol
+++ contracts/IMoneySafe.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.16;
 
+/**
+ * @title MoneySafe interface
+ * @author Edicha Joshua <mredichaj@gmail.com>
+ */
 interface IMoneySafe {
     /**
      * @dev emit when fund is saved by an account
@@ -17,18 +21,18 @@ interface IMoneySafe {
     event AccountRegistration(address indexed _addr);
 
     /**
-     * @dev emit during unauthorized activity
-     * @param _addr address that initiated the action
-     * @param _msg message describing the attempted action
-     */
-    event Unauthorized(address indexed _addr, string  _msg);
-
-    /**
      * @dev emit when duration is increased
      * @param _increase the number of days added
      * @param _total new duration
      */
     event DurationIncrease(uint256 _increase, uint256 _total);
+
+    /**
+     * @dev used with revert in the case of unauthorized activity
+     * @param _addr address that initiated the action
+     * @param _msg message describing the attempted action
+     */
+    error Unauthorized(address _addr, string  _msg);
 
     /**
      * @dev used with revert when zero is provided for amounts and days
@@ -45,11 +49,17 @@ interface IMoneySafe {
     error PrematureWithdrawal(uint256 _duration, uint256 _timeLeft, string _msg);
 
     /**
+     * @dev used with revert when account already exists
+     * @param _timeRegistered the time account was registered
+     */
+    error AlreadyRegistered(uint256 _timeRegistered);
+
+    /**
      * @dev register self and deposit first amount
      * @param _days duration in days
      * Note: should emit {AccountRegistration} event
      */
-    function start(uint8 _days) external payable;
+    function register(uint8 _days) external payable;
 
     /// @dev save new amount
     /// Note should save deposit details and emit {FundDeposit} event
@@ -92,7 +102,7 @@ interface IMoneySafe {
         uint256 balance; // in the MoneySafe token
         uint256 duration; // duration in days
         uint256 timeRegistered; // timestamp in milliseconds
-        DepositDetail[] depositDetails;
+        //DepositDetail[] depositDetails;
     }
 
     /**
